c47357880b44e719ccdb7e8ec59dee1c4fd1b9ad
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-20 00:13:52 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,23 +1,21 @@
+// Lib/firebase.ts
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/setup#available-libraries
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-};
-
 import { getAuth } from "firebase/auth";
+
+// Hardcoded Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDmDsi_JMQgx_QO4p8bnvfh-vKdN4Bmk8",
+  authDomain: "success-points.firebaseapp.com",
+  databaseURL: "https://success-points-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "success-points",
+  storageBucket: "success-points.firebasestorage.app",
+  messagingSenderId: "51177935348",
+  appId: "1:51177935348:web:33fc4a6810790a3cbd29a1",
+  measurementId: "G-64DR1TSTKY"
+};
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
