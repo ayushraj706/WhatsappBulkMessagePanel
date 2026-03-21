c72da70d76339f8d78b033b13a7761dae0495613
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-21 17:49:09 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Static files export karne ke liye zaroori hai
+  images: {
+    unoptimized: true, // Static export mein image optimization off rakhni padti hai
+  },
+  /* Baaki options yahan dalo agar hain toh */
 };
 
 export default nextConfig;
