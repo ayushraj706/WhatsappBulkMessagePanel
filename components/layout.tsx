2774d39c7427242135176e4b1dac313fc7314926
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-21 09:59:06 +0530

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,8 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className="antialiased">
+        {/* Is line se pure app ko light/dark mode ka signal milta hai */}
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
