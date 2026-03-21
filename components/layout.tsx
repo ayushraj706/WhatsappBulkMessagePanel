0f7ea07b62b290b9247407545009722ab692acbd
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-21 09:53:14 +0530

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        {/* Is line se pure app ko theme ka access milega */}
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
